aeb7a92647f31980fd2e10ed2758f94c75c4b165
ivand <ivan.dolgushin@lucidrealitylabs.com>
2019-12-27 00:38:46 +0200

    Basic implementation; no waiting returns supported

---
+++ EditorCoroutine.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public static class EditorCoroutine
+{
+	private static List<IEnumerator> coroutines;
+	static EditorCoroutine()
+	{
+		coroutines = new List<IEnumerator>();
+		EditorApplication.update += Update;
+	}
+
+	public static void StartCoroutine(IEnumerator coroutine)
+	{
+		coroutines.Add(coroutine);
+	}
+
+	public static void StopAllCoroutines()
+	{
+		coroutines.Clear();
+	}
+
+	private static void Update()
+	{
+		for (int i = coroutines.Count - 1; i >= 0; --i)
+		{
+			IEnumerator e = coroutines[i];
+			if (e.MoveNext())
+			{
+				// TODO: process return value here
+			}
+			else
+			{
+				coroutines.RemoveAt(i);
+			}
+		}
+	}
+}
